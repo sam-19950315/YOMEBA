2bb0e7807785f4954e10934752b1172f3009f203
sam-19950315 <isamucap@gmail.com>
2021-09-11 07:58:13 +0900

    deviseを使用したuser関連のバグ修正
    Userの登録を実施しようとすると、genre must existのエラーがでて登録ができないというエラー発生。
    
    モデル同士のgenreのrelationを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   mount_uploader :image, ImageUploader
 
-  belongs_to :genre
-  has_many :books
-  has_many :read_book_logs
+  belongs_to :genre, optional: true
 end

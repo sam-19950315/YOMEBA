ebb227a6e7111c4458f4a465f6d91488b8797436
sam-19950315 <isamucap@gmail.com>
2021-09-13 08:15:19 +0900

    本の未登録時のアラート
    # What
    本の未登録時のアラート
    # Why
    Userがエラーに気づくため

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,7 +10,22 @@ class BooksController < ApplicationController
   end
 
   def create
+<<<<<<< Updated upstream
     book = Book.create(book_params)
+=======
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to root_path
+    else  
+      flash.now[:error_book] = "タイトル、ジャンルは必須です。"
+      render :new
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+>>>>>>> Stashed changes
     redirect_to root_path
   end
 

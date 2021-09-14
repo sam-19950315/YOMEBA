165612d7a251f22a191f63ff57e5217bd04f5588
sam-19950315 <isamucap@gmail.com>
2021-09-14 19:19:43 +0900

    authentificationの設定
    # What
    authentificationの設定。
    ログインユーザー以外だと、ログインページに遷移する設定
    # Why
    エラー回避

---
+++ app/controllers/read_book_logs_controller.rb
@@ -0,0 +1,23 @@
+class ReadBookLogsController < ApplicationController
+  require "date"
+  before_action :authenticate_user!, only: [:create, :index, :genre_ranking, :graph]
+
+  def create
+    ReadBookLog.create(user_id: current_user.id, book_id: params[:id], log: Date.today)
+    redirect_to books_path
+  end
+
+  def index
+    @read_book_logs = Book.joins(:read_book_logs).joins(:genre).where(user_id: current_user.id).group("books.book_subject").group("genres.genre_name").order('count_all DESC').count.first(5)
+  end
+
+  def genre_ranking
+    @read_book_logs = Book.joins(:read_book_logs).joins(:genre).where(user_id: current_user.id).group("genres.genre_name").order('count_all DESC').count.first(5)
+  end
+
+  def graph
+    @datas = ReadBookLog.where(user_id: current_user.id).select(:log).group("read_book_logs.log").count.first(30)
+
+  end
+
+end

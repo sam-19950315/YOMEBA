97981762f6ead47717dbc2e65096cebef55aa50f
sam-19950315 <isamucap@gmail.com>
2021-09-14 13:21:54 +0900

    ジャンルごとのランキング作成機能実装
    # What
    ジャンルごとのランキング作成機能実装
    # Why
    お子様の興味を分析するため

--- app/controllers/read_book_logs_controller.rb
+++ app/controllers/read_book_logs_controller.rb
@@ -6,20 +6,13 @@ class ReadBookLogsController < ApplicationController
     redirect_to books_path
   end
 
-  def book_ranking
-    @books_data = Book.where(user_id: current_user.id)
-    books_count = []
-    @books_data.each do |book|
-      books_count_detail = []
-      books_count_detail << book.read_book_logs.count
-      books_count_detail << book.book_subject
-      books_count_detail << book.genre_id
-      books_count << books_count_detail
-    end
-    @books = books_count.sort.reverse
-    @genre = Genre.all
+  def index
+    @read_book_logs = Book.joins(:read_book_logs).joins(:genre).where(user_id: current_user.id).group("books.book_subject").group("genres.genre_name").order('count_all DESC').count.first(5)
   end
 
+  def genre_ranking
+    @read_book_logs = Book.joins(:read_book_logs).joins(:genre).where(user_id: current_user.id).group("genres.genre_name").order('count_all DESC').count.first(5)
+  end
 
 
 end

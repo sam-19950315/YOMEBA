a880c7aa688a2cddb5ad896c43ce17cda8a23dc0
sam-19950315 <isamucap@gmail.com>
2021-09-14 19:31:29 +0900

    authentificationの設定
    # What
    authentificationの設定。
    ログインユーザー以外だと、ログインページに遷移する設定
    # Why
    エラー回避

--- app/controllers/trashes_controller.rb
+++ app/controllers/trashes_controller.rb
@@ -1,5 +1,5 @@
 class TrashesController < ApplicationController
-  before_action :authenticate_user!, only: [:index, :trash_reverse]
+  before_action :authenticate_user!, only: [:index, :reverse]
 
   def index
     @books = Book.where(user_id: current_user.id, is_deleted: true)

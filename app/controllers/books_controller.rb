96869248e83426935bb8edeabe8c9484b27ccf49
sam-19950315 <isamucap@gmail.com>
2021-09-27 23:43:37 +0900

    feat:RakutenBooksAPI導入
    # What
    RakutenBooksAPI導入
    # Why
    新しい本が登録された際、
    一覧ページに絵本の表紙が表示される様にし、すぐに視認できる様にするため。

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,13 +4,32 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.where(user_id: current_user.id,is_deleted: false)
+    @book_image_url = []
+    @books.each do |book|
+      book_data = RakutenWebService::Books::Book.search(title: book.book_subject)
+      if book_data.count == 0
+        @book_image_url << nil
+      else
+        book_data.each_with_index do |book_url,i|
+          if i == 0 
+            book_url_a_data = book_url.large_image_url
+            @book_image_url << book_url_a_data
+          end
+        end
+      end
+    end
   end
 
   def new
     @book = Book.new
+    if params[:keyword]
+      @books = RakutenWebService::Books::Book.search(title: params[:keyword])
+      render :new
+    end
   end
 
   def create
+    binding.pry
     @book = Book.new(book_params)
     if @book.save
       flash[:success_book_registration] = CREATE_BOOK_SUCCESS
@@ -20,6 +39,14 @@ class BooksController < ApplicationController
       render :new
     end
   end
+
+  def search
+    if params[:keyword]
+      @books = RakutenWebService::Books::Book.search(title: params[:keyword])
+    end
+  end
+
+
 
   def destroy
     ActiveRecord::Base.transaction do
